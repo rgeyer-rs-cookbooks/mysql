a8658df9dff6046415c3e17ca676e3dee6177d24
Ryan J. Geyer <me@ryangeyer.com>
2013-02-04 17:01:20 -0800

    Better handling of provider update to hide deprecation warning

--- providers/database.rb
+++ providers/database.rb
@@ -3,23 +3,32 @@ include Opscode::Mysql::Database
 action :flush_tables_with_read_lock do
   Chef::Log.info "mysql_database: flushing tables with read lock"
   db.query "flush tables with read lock"
-  new_resource.updated_by_last_action(true) if new_resource.respond_to? 'updated_by_last_action'
-  new_resource.updated = true if new_resource.updated
+  if new_resource.respond_to? 'updated_by_last_action'
+    new_resource.updated_by_last_action(true)
+  else
+    new_resource.updated = true
+  end
 end
 
 action :unflush_tables do
   Chef::Log.info "mysql_database: unlocking tables"
   db.query "unlock tables"
-  new_resource.updated_by_last_action(true) if new_resource.respond_to? 'updated_by_last_action'
-  new_resource.updated = true if new_resource.updated
+  if new_resource.respond_to? 'updated_by_last_action'
+    new_resource.updated_by_last_action(true)
+  else
+    new_resource.updated = true
+  end
 end
 
 action :create_db do
   unless @mysqldb.exists
     Chef::Log.info "mysql_database: Creating database #{new_resource.database}"
     db.query("create database #{new_resource.database}")
-    new_resource.updated_by_last_action(true) if new_resource.respond_to? 'updated_by_last_action'
-    new_resource.updated = true if new_resource.updated
+    if new_resource.respond_to? 'updated_by_last_action'
+      new_resource.updated_by_last_action(true)
+    else
+      new_resource.updated = true
+    end
   end
 end
 
